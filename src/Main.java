fa822b47a8d6b68ecd91118b734e652b5e805d6b
g21turbo <dg21stew@gmail.com>
2023-11-05 13:16:16 -0800

    Added isValid method

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,44 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(hasSameLastDigit(11, 12, 13));
-        System.out.println(hasSameLastDigit(9, 12, 13));
         System.out.println(hasSameLastDigit(10, 12, 1000));
-        System.out.println(hasSameLastDigit(10, 12, 1001));
+        System.out.println(hasSameLastDigit(12, 22, 11));
+        System.out.println(hasSameLastDigit(10, 22, 992));
+        System.out.println(hasSameLastDigit(10, 20, 50));
+        System.out.println(hasSameLastDigit(9, 20, 50));
+        System.out.println(hasSameLastDigit(10, 20, 1001));
+        System.out.println(hasSameLastDigit(9, 99, 19));
 
+    }
+
+    public static boolean isValid(int number){
+       if (number >= 10 && number <= 1000) {
+           return true;
+       }
+       return false;
     }
 
     public static boolean hasSameLastDigit(int num1, int num2, int num3) {
 
-
         // Input validation
-        boolean num1Valid = num1 >= 10 && num1 <= 1000;
-        boolean num2Valid = num2 >= 10 && num2 <= 1000;
-        boolean num3Valid = num3 >= 10 && num3 <= 1000;
-
-        if (!num1Valid || !num2Valid || !num3Valid) {
+        if (!isValid(num1) || !isValid(num2) || !isValid(num3)) {
             return false;
         }
 
-        return true;
+        // Assign variable to the last digit in each number
+        int lastDigitNum1 = num1 % 10;
+        int lastDigitNum2 = num2 % 10;
+        int lastDigitNum3 = num3 % 10;
 
+        // Compares the last digit in each number
+        boolean sharedDigit = lastDigitNum1 == lastDigitNum2 ||
+                                lastDigitNum1 == lastDigitNum3;
+        boolean sharedDigitTwo = lastDigitNum2 == lastDigitNum3;
+
+        // returns true is at least two on the last digits match
+        if (sharedDigit || sharedDigitTwo) {
+            return true;
+        }
+        return false;
     }
-
-
 }
